b4b4befbb5b3313b7b63f932e903d05c8e8982ab
munene5470 <munenemugeki@gmail.com>
2019-08-27 11:27:43 +0300

    update

--- admin/reportupload.php
+++ admin/reportupload.php
@@ -1,29 +1,29 @@
 
 <?php
+   $connect = mysqli_connect("localhost", "root",
+   "", "county");
+   $query = "SELECT * FROM reports ORDER BY id desc";
+   $result = mysqli_query($connect,$query);
+?>
+<!DOCTYPE html>
+   <html>
+       <head>
+           <script src="vendor/jquery/jquery.min.js" type="6b6825c0d26fda4be87006a1-text/javascript"></script>
+           <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+       </head>
+       <body>
+           <br/><br/>
+           <br/><br/>
+           <div class ="container" style="width:900px;">
+           <h1 align="center">Export Mysql Table to csv file in php</h1>
 
-// Database Connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "county";
- 
- $conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-    ?>
-	<div class="container">
-	<a href="export.php?export=true">Export</a>
-	
-	<table>
-	  <tr>
-	    
-	  
-	  
-	  
-	  
-	  </tr>
-	</table>
+
+
+
+       </body>
+   </html>
+
+
 	
 	
 	
